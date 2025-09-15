1d331051c33303487c570686aaca5ba8edce330b
Phil Larson <hello@phillarson.xyz>
2025-09-15 10:22:30 -0700

    darwin

--- flake.nix
+++ flake.nix
@@ -5,34 +5,75 @@
   };
   outputs = { self, nixpkgs }: 
     let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-      fhs = pkgs.buildFHSUserEnv {
-        name = "nvm-env";
-        targetPkgs = pkgs: with pkgs; [
-          udev
-          glibc
-          gcc
-          zsh
-          vim
-          git
-        ];
-        runScript = "zsh";
-        profile = ''
-          if [[ -x "NVM_DIR" ]]; then
-            NVM_DIR="$HOME/.nvm"
-            echo "Using default NVM_DIR: $NVM_DIR"
-          fi
-          export NVM_DIR
-          unset LD_LIBRARY_PATH
-          if [ ! -d "$NVM_DIR" ]; then
-            git clone https://github.com/nvm-sh/nvm.git ~/.nvm
-          fi
-          [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-        '';
-      };
-  in {
-    devShells.${system}.default = fhs.env;
-  };
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in {
+      devShells = forAllSystems (system: 
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          isDarwin = pkgs.stdenv.isDarwin;
+          
+          # Platform-specific packages
+          linuxPkgs = with pkgs; lib.optionals (!isDarwin) [
+            udev
+            glibc
+          ];
+          
+          commonPkgs = with pkgs; [
+            gcc
+            zsh
+            vim
+            git
+            curl
+            which
+          ];
+          
+          # Use buildFHSUserEnv on Linux, regular mkShell on Darwin
+          shell = if isDarwin then
+            pkgs.mkShell {
+              buildInputs = commonPkgs;
+              shellHook = ''
+                export NVM_DIR="$HOME/.nvm"
+                echo "Using NVM_DIR: $NVM_DIR"
+                if [ ! -d "$NVM_DIR" ]; then
+                  echo "Installing nvm..."
+                  git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR"
+                fi
+                if [ -s "$NVM_DIR/nvm.sh" ]; then
+                  source "$NVM_DIR/nvm.sh"
+                  echo "nvm loaded successfully"
+                fi
+                if [ -s "$NVM_DIR/bash_completion" ]; then
+                  source "$NVM_DIR/bash_completion"
+                fi
+              '';
+            }
+          else
+            (pkgs.buildFHSUserEnv {
+              name = "nvm-env";
+              targetPkgs = pkgs: commonPkgs ++ linuxPkgs;
+              runScript = "zsh";
+              profile = ''
+                export NVM_DIR="$HOME/.nvm"
+                echo "Using NVM_DIR: $NVM_DIR"
+                unset LD_LIBRARY_PATH
+                if [ ! -d "$NVM_DIR" ]; then
+                  echo "Installing nvm..."
+                  git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR"
+                fi
+                if [ -s "$NVM_DIR/nvm.sh" ]; then
+                  source "$NVM_DIR/nvm.sh"
+                  echo "nvm loaded successfully"
+                fi
+                if [ -s "$NVM_DIR/bash_completion" ]; then
+                  source "$NVM_DIR/bash_completion"
+                fi
+              '';
+            }).env;
+        in {
+          default = shell;
+        }
+      );
+    };
 }
 
